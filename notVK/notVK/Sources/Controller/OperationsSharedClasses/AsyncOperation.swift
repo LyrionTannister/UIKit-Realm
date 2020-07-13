266e8d07e86e31df4e4886da25f14af030ab855d
LyrionTannister <loudpiper@gmail.com>
2020-07-13 16:13:11 +0300

    Added AsyncOperation shared class

---
+++ notVK/notVK/Sources/Controller/OperationsSharedClasses/AsyncOperation.swift
@@ -0,0 +1,58 @@
+//
+//  AsyncOperation.swift
+//  notVK
+//
+//  Created by Roman on 13.07.2020.
+//  Copyright © 2020 DrewMyScreen. All rights reserved.
+//
+
+import Foundation
+
+class AsyncOperation: Operation {
+    
+    enum State: String {
+        case ready, executing, finished
+        fileprivate var keyPath: String {
+            return "is" + rawValue.capitalized
+        }
+    }
+    
+    var state = State.ready {
+        willSet {
+            willChangeValue(forKey: state.keyPath)
+            willChangeValue(forKey: newValue.keyPath)
+        }
+        didSet {
+            didChangeValue(forKey: state.keyPath)
+            didChangeValue(forKey: oldValue.keyPath)
+        }
+    }
+    
+    override var isAsynchronous: Bool {
+        return true
+    }
+    
+    override var isReady: Bool {
+        return super.isReady && state == .ready
+    }
+    
+    override var isExecuting: Bool {
+        return state == .executing
+    }
+    override var isFinished: Bool {
+        return state == .finished
+    }
+    override func start() {
+        if isCancelled {
+            state = .finished
+        } else {
+            main()
+            state = .executing
+        }
+    }
+    override func cancel() {
+        super.cancel()
+        state = .finished
+    }
+}
+
