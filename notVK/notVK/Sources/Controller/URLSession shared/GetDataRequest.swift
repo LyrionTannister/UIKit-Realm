7210c5519a4e6bf19f6c4423290b73ca055cd61e
LyrionTannister <loudpiper@gmail.com>
2020-07-16 14:19:42 +0300

    Added GetDataRequestDelegate class for GetDataRequest

--- notVK/notVK/Sources/Controller/URLSession shared/GetDataRequest.swift
+++ notVK/notVK/Sources/Controller/URLSession shared/GetDataRequest.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class GetDataRequest: NSObject {
+    
+    typealias DownloaderCH = (URL?) -> ()
     let config: URLSessionConfiguration
+    
     lazy var session: URLSession = {
         return URLSession(configuration: self.config,
                           delegate: GetDataRequestDelegate(),
                           delegateQueue: .main)
     }()
+    
     init(configuration config: URLSessionConfiguration) {
         self.config = config
         super.init()
     }
-    // ...
+    
+    @discardableResult
+    func getData(url: URL,
+                 completionHandler ch : @escaping DownloaderCH) -> URLSessionTask {
+     let task = self.session.downloadTask(with: url)
+     // ... store the completion function somehow ...
+     task.resume()
+     return task
+    }
+
+    
     deinit {
         self.session.invalidateAndCancel()
     }
---
+++ notVK/notVK/Sources/Controller/URLSession shared/GetDataRequestDelegate.swift
@@ -0,0 +1,59 @@
+//
+//  GetDataRequestDelegate.swift
+//  notVK
+//
+//  Created by Roman on 16.07.2020.
+//  Copyright © 2020 DrewMyScreen. All rights reserved.
+//
+
+import UIKit
+
+class GetDataRequestDelegate: URLSessionDelegate {
+    func isEqual(_ object: Any?) -> Bool {
+        <#code#>
+    }
+    
+    var hash: Int = 0
+    
+    var superclass: AnyClass?
+    
+    func `self`() -> Self {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func isProxy() -> Bool {
+        <#code#>
+    }
+    
+    func isKind(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func isMember(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func conforms(to aProtocol: Protocol) -> Bool {
+        <#code#>
+    }
+    
+    func responds(to aSelector: Selector!) -> Bool {
+        <#code#>
+    }
+    
+    var description: String = ""
+    
+    //TODO: Realize
+}
