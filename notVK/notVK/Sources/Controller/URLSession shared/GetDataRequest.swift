95023654a1455fdc630bbdfd7b159a133d03397b
LyrionTannister <loudpiper@gmail.com>
2020-07-16 17:04:06 +0300

    Intermediate commit

--- notVK/notVK/Sources/Controller/URLSession shared/GetDataRequest.swift
+++ notVK/notVK/Sources/Controller/URLSession shared/GetDataRequest.swift
@@ -8,9 +8,27 @@
 
 import UIKit
 
+let downloader: GetDataRequest = {
+ let config = URLSessionConfiguration.default
+    config.timeoutIntervalForRequest = 30
+ return GetDataRequest(configuration: config)
+}()
+
+let s = "https://www.someserver.com/somefolder/someimage.jpg"
+let url = URL(string:s)!
+let req = URLRequest(url: url)
+downloader.getData(urlRequest: req) { url in
+    if let url = url, let d = try? Data(contentsOf: url) {
+        let im = UIImage(data: d)
+        print(im) // assume we're called back on main thread
+    }
+}
+
 class GetDataRequest: NSObject {
     
-    typealias DownloaderCH = (URL?) -> ()
+    typealias DownloaderCH = (URLRequest?) -> ()
+    
+    var data = [Int:Data]()
     let config: URLSessionConfiguration
     
     lazy var session: URLSession = {
@@ -25,16 +43,48 @@ class GetDataRequest: NSObject {
     }
     
     @discardableResult
-    func getData(url: URL,
+    func getData(urlRequest: URLRequest,
                  completionHandler ch : @escaping DownloaderCH) -> URLSessionTask {
-     let task = self.session.downloadTask(with: url)
-     // ... store the completion function somehow ...
-     task.resume()
-     return task
+        
+        let task = self.session.dataTask(with: urlRequest)
+        let delegate = self.session.delegate!
+        delegate.appendHandler(ch, task: task)
+        
+        task.resume()
+        return task
     }
-
+    
     
     deinit {
         self.session.invalidateAndCancel()
     }
+}
+
+class GetDataRequestDelegate: NSObject, URLSessionDelegate {
+    
+    typealias GetDataCH = (URLRequest?) -> ()
+    
+    private var handlers = [Int:GetDataCH]()
+    
+    func appendHandler(_ ch: @escaping GetDataCH, task: URLSessionTask) {
+        self.handlers[task.taskIdentifier] = ch
+    }
+    
+    func urlSession(_ session: URLSession,
+                    dataTask getDataTask: URLSessionDataTask,
+                    didReceive data: Data) {
+        let ch = self.handlers[getDataTask.taskIdentifier]
+        
+    }
+    
+    func urlSession(_ session: URLSession,
+                    task: URLSessionTask,
+                    didCompleteWithError error: Error?) {
+        let ch = self.handlers[task.taskIdentifier]
+        self.handlers[task.taskIdentifier] = nil
+        if error == nil {
+            ch?(nil)
+        }
+    }
+
 }
--- notVK/notVK/Sources/Controller/URLSession shared/GetDataRequestDelegate.swift
+++ notVK/notVK/Sources/Controller/URLSession shared/GetDataRequestDelegate.swift
@@ -8,52 +8,31 @@
 
 import UIKit
 
-class GetDataRequestDelegate: URLSessionDelegate {
-    func isEqual(_ object: Any?) -> Bool {
-        <#code#>
-    }
-    
-    var hash: Int = 0
-    
-    var superclass: AnyClass?
-    
-    func `self`() -> Self {
-        <#code#>
-    }
-    
-    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
-        <#code#>
-    }
-    
-    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
-        <#code#>
-    }
-    
-    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
-        <#code#>
-    }
-    
-    func isProxy() -> Bool {
-        <#code#>
-    }
-    
-    func isKind(of aClass: AnyClass) -> Bool {
-        <#code#>
-    }
-    
-    func isMember(of aClass: AnyClass) -> Bool {
-        <#code#>
-    }
-    
-    func conforms(to aProtocol: Protocol) -> Bool {
-        <#code#>
-    }
-    
-    func responds(to aSelector: Selector!) -> Bool {
-        <#code#>
-    }
-    
-    var description: String = ""
-    
-    //TODO: Realize
-}
+//class GetDataRequestDelegate: NSObject, URLSessionDelegate {
+//    
+//    typealias GetDataCH = (URLRequest?) -> ()
+//    
+//    private var handlers = [Int:GetDataCH]()
+//    
+//    func appendHandler(_ ch: @escaping GetDataCH, task: URLSessionTask) {
+//        self.handlers[task.taskIdentifier] = ch
+//    }
+//    
+//    func urlSession(_ session: URLSession,
+//                    dataTask getDataTask: URLSessionDataTask,
+//                    didReceive data: Data) {
+//        let ch = self.handlers[getDataTask.taskIdentifier]
+//        
+//    }
+//    
+//    func urlSession(_ session: URLSession,
+//                    task: URLSessionTask,
+//                    didCompleteWithError error: Error?) {
+//        let ch = self.handlers[task.taskIdentifier]
+//        self.handlers[task.taskIdentifier] = nil
+//        if error == nil {
+//            ch?(nil)
+//        }
+//    }
+//
+//}
